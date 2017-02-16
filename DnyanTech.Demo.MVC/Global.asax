a242c8521c0a4c6a6e6937654880b6399eeff699
aniketjadhav10 <aniketjadhav10@gmail.com>
2017-02-16 23:10:16 +0530

    Add project files.

---
+++ DnyanTech.Demo.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DnyanTech.Demo.MVC.MvcApplication" Language="C#" %>
---
+++ DnyanTech.Demo.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DnyanTech.Demo.MVC.Startup))]
+namespace DnyanTech.Demo.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
